c63b4fea158689cec26ba86e75a0a79e0543693d
JuMalIO <spamjulius@mail.com>
2018-12-25 13:14:48 +0200

    CreateWindow; check is on battery power; turn on led on device connected to PC

--- CapsLockLed/main.cpp
+++ CapsLockLed/main.cpp
@@ -1,8 +1,11 @@
 #include <windows.h>
-#include <winioctl.h>
 #include <dinput.h>
-#include <ntsecapi.h>
-#include <string>
+#include <Dbt.h>
+#include <stdexcept>
+
+#define HID_CLASSGUID {0x4d1e55b2, 0xf16f, 0x11cf, {0x88, 0xcb, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30}}
+#define WND_NAME "CAPS_LOCK_LED"
+#define HWND_MESSAGE     ((HWND)-3)
 
 #pragma comment(linker, "/subsystem:windows")
 
@@ -47,9 +50,22 @@ void KeybdLight(DWORD wFlags)
 	CloseHandle(hKeybd);
 }
 
+bool IsOnBatteryPower()
+{
+	SYSTEM_POWER_STATUS lpSysPwrStatus;
+	if (GetSystemPowerStatus(&lpSysPwrStatus))
+	{
+		if (lpSysPwrStatus.ACLineStatus == 0)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 const DWORD DOUBLE_TAP_MILLISECONDS = 400;
 
-DWORD last_time = 0;
+DWORD _last_time = 0;
 
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -59,14 +75,14 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 	{
 		if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP)
 		{
-			if (kbdllhookstruct->time - last_time < DOUBLE_TAP_MILLISECONDS)
+			if (kbdllhookstruct->time - _last_time < DOUBLE_TAP_MILLISECONDS)
 			{
 				KeybdLight(KBD_CAPS | KBD_OFF);
 				PostQuitMessage(0);
 				return 0;
 			}
 
-			last_time = kbdllhookstruct->time;
+			_last_time = kbdllhookstruct->time;
 		}
 
 		KeybdLight(KBD_CAPS | KBD_ON);
@@ -74,24 +90,85 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-DWORD flashSleepMilliseconds = 1000;
+bool _flashEnabled = false;
+DWORD _flashSleepMilliseconds = 1000;
 
 void Flash()
 {
 	for (;;)
 	{
-		Sleep(flashSleepMilliseconds);
+		Sleep(_flashSleepMilliseconds);
 		KeybdLight(KBD_CAPS);
 	}
 }
 
-int WINAPI WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nShowCmd)
+LRESULT WndProc(HWND__* hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
-	KeybdLight(KBD_CAPS | KBD_ON);
+	switch (iMsg)
+	{
+		case WM_NCCREATE:
+		{
+			return true;
+		}
+		case WM_CREATE:
+		{
+			LPCREATESTRUCT params = (LPCREATESTRUCT)lParam;
+			GUID InterfaceClassGuid = *((GUID*)params->lpCreateParams);
+			DEV_BROADCAST_DEVICEINTERFACE NotificationFilter;
+			ZeroMemory(&NotificationFilter, sizeof(NotificationFilter));
+			NotificationFilter.dbcc_size = sizeof(DEV_BROADCAST_DEVICEINTERFACE);
+			NotificationFilter.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
+			NotificationFilter.dbcc_classguid = InterfaceClassGuid;
+			HDEVNOTIFY dev_notify = RegisterDeviceNotification(hwnd, &NotificationFilter,
+				DEVICE_NOTIFY_WINDOW_HANDLE);
+			if (dev_notify == NULL)
+			{
+				throw std::runtime_error("Could not register for device notifications!");
+			}
 
+			HPOWERNOTIFY power_notify = RegisterPowerSettingNotification(hwnd, &GUID_ACDC_POWER_SOURCE,
+				DEVICE_NOTIFY_WINDOW_HANDLE);
+			if (power_notify == NULL)
+			{
+				throw std::runtime_error("Could not register for power notifications!");
+			}
+			break;
+		}
+		case WM_DEVICECHANGE:
+		{
+			PDEV_BROADCAST_HDR lpdb = (PDEV_BROADCAST_HDR)lParam;
+			if (lpdb != NULL && lpdb->dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE)
+			{
+				if (wParam == DBT_DEVICEARRIVAL || wParam == DBT_DEVICEREMOVECOMPLETE)
+				{
+					KeybdLight(KBD_CAPS | KBD_ON);
+				}
+			}
+			break;
+		}
+		case WM_CLOSE:
+		{
+			KeybdLight(KBD_CAPS | KBD_OFF);
+			break;
+		}
+		case WM_POWERBROADCAST:
+		{
+			if (wParam == PBT_APMPOWERSTATUSCHANGE)
+			{
+				if (IsOnBatteryPower())
+				{
+					KeybdLight(KBD_CAPS | KBD_OFF);
+					PostQuitMessage(0);
+				}
+			}
+			break;
+		}
+	}
+	return 0;
+}
+
+void ReadArguments()
+{
 	LPWSTR *szArgList;
 	int argCount;
 
@@ -109,25 +186,67 @@ int WINAPI WinMain(HINSTANCE hInstance,
 					int milliseconds = _wtoi(szArgList[i + 1]);
 					if (milliseconds > 0)
 					{
-						flashSleepMilliseconds = milliseconds;
+						_flashSleepMilliseconds = milliseconds;
 					}
 				}
 
-				CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Flash, 0, 0, 0);
+				_flashEnabled = true;
 			}
 		}
 	}
 
 	LocalFree(szArgList);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd)
+{
+	ReadArguments();
+
+	if (IsOnBatteryPower())
+	{
+		return 0;
+	}
+
+	KeybdLight(KBD_CAPS | KBD_ON);
+
+	if (_flashEnabled)
+	{
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Flash, 0, 0, 0);
+	}
 
 	SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, GetModuleHandle(0), 0);
 
+	WNDCLASSEX wx;
+	ZeroMemory(&wx, sizeof(wx));
+	wx.cbSize = sizeof(WNDCLASSEX);
+	wx.lpfnWndProc = reinterpret_cast<WNDPROC>(WndProc);
+	wx.style = CS_HREDRAW | CS_VREDRAW;
+	wx.hInstance = GetModuleHandle(0);
+	wx.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+	wx.lpszClassName = WND_NAME;
+
+	HWND hWnd = NULL;
+
+	if (RegisterClassEx(&wx))
+	{
+		GUID guid = HID_CLASSGUID;
+		hWnd = CreateWindow(WND_NAME, WND_NAME, WS_ICONIC, 0, 0, 0, 0,
+			NULL, NULL, GetModuleHandle(0), (void*)&guid);
+	}
+
+	if (hWnd == NULL)
+	{
+		throw std::runtime_error("Could not create message window!");
+	}
+
 	MSG msg;
-	while (GetMessage(&msg, 0, 0, 0) > 0)
+	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-
 	return 0;
 }
